d92193b22841afa15f8e8ab39498104151d0790b
Karina Espinola <k.espinolah@gmail.com>
2021-12-31 18:14:27 -0600

    Add database connection

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 
-app.listen(PORT, () => {
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
+  });
 });
